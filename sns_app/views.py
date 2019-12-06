5cfa6006859eac5dd6e9604afdf1ef8d6bff6b0a
wata-pom <w.kumagai1224@gmail.com>
2019-12-06 17:26:58 +0900

    modelから作られるobjectについて
    udemy 55まで終了

--- sns_app/views.py
+++ sns_app/views.py
@@ -1,6 +1,15 @@
 # Create your views here.
+from django.contrib.auth.models import User
 from django.shortcuts import render
 
 
 def signupfunc(request):
+    user2 = User.objects.get(username='wataru')
+    print(user2.email)
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = User.objects.create_user(username, '', password)
+        return render(request, 'signup.html', {'some': 100})
     return render(request, 'signup.html', {'some': 100})
