7699ea501ff081ce8334f37a90f2ca6b127bf230
wata-pom <w.kumagai1224@gmail.com>
2019-12-10 02:31:15 +0900

    signup.htmlの見た目を修正
     -static dirの設定
     -modelの設定
     -その他、多数

--- sns_app/models.py
+++ sns_app/models.py
@@ -1,1 +1,12 @@
 # Create your models here.
+from django.db import models
+
+
+class SnsModel(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    contributor = models.CharField(max_length=50)
+    image = models.ImageField(upload_to='')
+    good = models.IntegerField()
+    read = models.IntegerField()
+    readtext = models.CharField(max_length=100)
--- sns_app/views.py
+++ sns_app/views.py
@@ -1,15 +1,34 @@
 # Create your views here.
+from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def signupfunc(request):
-    user2 = User.objects.get(username='wataru')
-    print(user2.email)
-
     if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        user = User.objects.create_user(username, '', password)
-        return render(request, 'signup.html', {'some': 100})
+        username2 = request.POST['username']
+        password2 = request.POST['password']
+        try:
+            User.objects.get(username=username2)
+            return render(request, 'signup.html', {'erorr': '※このユーザーは登録されています。'})
+        except:
+            user = User.objects.create_user(username2, '', password2)
+            return render(request, 'signup.html', {'some': 100})
     return render(request, 'signup.html', {'some': 100})
+
+
+def loginfunc(request):
+    if request.method == 'POST':
+        username2 = request.POST['username']
+        password2 = request.POST['password']
+        user = authenticate(request, username=username2, password=password2)
+        if user is not None:
+            login(request, user)
+            return render(request, 'signup.html')
+        else:
+            return redirect('login')
+    return render(request, 'login.html')
+
+
+def listfunc(request):
+    return render(request, 'list.html')
